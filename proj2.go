0c740a9e94f09dbc6ccbca3dcb9e0d5748d62944
justinqu@berkeley.edu <interns@openpath.io>
2021-03-23 22:01:48 -0700

    test

--- proj2.go
+++ proj2.go
@@ -36,6 +36,9 @@ import (
 // a) It shows you some useful primitives, and
 // b) it suppresses warnings for items not being imported.
 // Of course, this function can be deleted.
+
+//test
+
 func someUsefulThings() {
 	// Creates a random UUID
 	f := uuid.New()
